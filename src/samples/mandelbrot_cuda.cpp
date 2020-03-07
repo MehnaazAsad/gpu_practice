e19ffc50521bcef3d884a13a50ff989f7e15f6e7
MehnaazAsad <mehnaaz.asad@vanderbilt.edu>
2020-03-07 17:11:32 -0600

    Added assert statements to ensure success

--- src/samples/mandelbrot_cuda.cpp
+++ src/samples/mandelbrot_cuda.cpp
@@ -32,6 +32,8 @@ clean and free up colour array malloc
 #include <stdlib.h>
 #include <time.h>
 #include <CL/cl.h>
+#include <assert.h>
+#include <chrono>
 #include "samples/mandelbrot.h"
 
 #define MAX_SOURCE_SIZE (0x100000) //macro substitution
@@ -69,49 +71,64 @@ int mandelbrot_cuda(void)
 	cl_uint ret_num_devices;
 	cl_uint ret_num_platforms;
 	cl_int ret = clGetPlatformIDs(1, &platform_id, &ret_num_platforms);
+	assert(ret == CL_SUCCESS);
 	ret = clGetDeviceIDs(platform_id, CL_DEVICE_TYPE_DEFAULT, 1,
 		&device_id, &ret_num_devices);
+	assert(ret == CL_SUCCESS);
 
 	// Create an OpenCL context
 	cl_context context = clCreateContext(NULL, 1, &device_id, NULL, NULL, &ret);
+	assert(ret == CL_SUCCESS);
 
 	// Create a command queue
 	cl_command_queue command_queue = clCreateCommandQueueWithProperties(context, device_id, 0, &ret);
+	assert(ret == CL_SUCCESS);
 
 	// Create memory buffers on the device (GPU) for each vector 
 	cl_mem image_obj = clCreateBuffer(context, CL_MEM_WRITE_ONLY,
 		mandelbrot.size_in_bytes(), NULL, &ret);
+	assert(ret == CL_SUCCESS);
 
 	// Create a program from the kernel source
 	cl_program program = clCreateProgramWithSource(context, 1,
 		(const char**)&source_str, (const size_t*)&source_size, &ret);
+	assert(ret == CL_SUCCESS);
 
 	// Build the program
 	ret = clBuildProgram(program, 1, &device_id, NULL, NULL, NULL);
+	assert(ret == CL_SUCCESS);
 
 	// Create the OpenCL kernel
 	cl_kernel kernel = clCreateKernel(program, "mandelbrot", &ret);
+	assert(ret == CL_SUCCESS);
 
 	// Set the arguments of the kernel
 	ret = clSetKernelArg(kernel, 0, sizeof(cl_mem), (void*)&image_obj);
+	assert(ret == CL_SUCCESS);
 
-	clock_t start = clock();
+	using namespace std::chrono;
+	high_resolution_clock::time_point t1 = high_resolution_clock::now();
+	//clock_t start = clock();
 	// Execute the OpenCL kernel on the list
-	size_t global_item_size = mandelbrot.size(); // Process the entire lists
+	size_t global_item_size = mandelbrot.size(); // Process entire image
 	size_t local_item_size = 256; // Divide work items into groups of 256
 	ret = clEnqueueNDRangeKernel(command_queue, kernel, 1, NULL,
 		&global_item_size, &local_item_size, 0, NULL, NULL);
+	assert(ret==0);
 	
 	// Read GPU to CPU
 	ret = clEnqueueReadBuffer(command_queue, image_obj, CL_TRUE, 0,
 		mandelbrot.size_in_bytes(), mandelbrot.colour_arr_start, 0, NULL, NULL);
+	assert(ret == CL_SUCCESS);
 	
-	clock_t finish = clock();
-	float time_spent = (float)(finish - start) / CLOCKS_PER_SEC;
+	high_resolution_clock::time_point t2 = high_resolution_clock::now();
+	duration<double> time_span = duration_cast<duration<double>>(t2 - t1);
+	//clock_t finish = clock();
+	//float time_spent = (float)(finish - start) / CLOCKS_PER_SEC;
 	FILE* output_time;
 	fopen_s(&output_time, "timing.txt", "w");
 	fprintf(output_time, "OpenCL (s)\n");
-	fprintf(output_time, "%d %d %d %f\n", mandelbrot.width, mandelbrot.height, max_iteration, time_spent);
+	fprintf(output_time, "%d %d %d %lf\n", mandelbrot.width, mandelbrot.height, max_iteration, time_span.count());
 	fclose(output_time);
 
 	FILE* output_image;
